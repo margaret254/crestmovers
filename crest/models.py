e92304e77c3b4d6337bdab40bd8a431bb8c4e3c9
Margaret mwangi <maggiemwas91@gmail.com>
2020-02-24 14:53:32 +0300

    Add models,views,templates and statics

--- crest/models.py
+++ crest/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+# About Model
+
+class About(models.Model):
+    description = models.TextField()
+    values = models.TextField()
+    image = models.ImageField(upload_to="about")
+
+    class Meta:
+        verbose_name = "About me"
+        verbose_name_plural = "About me"
+
+    def __str__(self):
+        return "About us"
+
+# Recent Work Model
+
+class RecentWork(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Work title")
+    short_description = models.TextField()
+    image = models.ImageField(upload_to="works")
+
+    def __str__(self):
+        return self.title
+
+
